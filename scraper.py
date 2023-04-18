00a6ff8e89bf43dce614addf2b8553422c9af4c1
Hlib Biovetskyi <227938@student.uek.krakow.pl>
2023-04-18 16:19:29 +0200

    2023-04-18 HB

--- scraper.py
+++ scraper.py
@@ -2,8 +2,17 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-# product_code = input("Please enter the product code: ")
-product_code = "129910940"
+def get_element(dom_tree, selector = None, attribute = None):
+    try:
+        if attribute:
+            if selector:
+                return dom_tree[attribute]
+            return dom_tree.select_one(selector).text.strip
+    except AttributeError:
+        return None
+
+product_code = input("Please enter the product code: ")
+# product_code = "129910940"
 # print(product_code)
 # product_code = "150607728"
 
@@ -14,30 +23,28 @@ while url:
     response = requests.get(url)
     if response.status_code == requests.codes.ok:
         page_dom = BeautifulSoup(response.text, "html.parser")
+        print(get_element(page_dom))
         opinions = page_dom.select("div.js_product-review")
         if len(opinions)>0:
             
             print(f"There are some opinions about product with {product_code} code.")
             for opinion in opinions:
                 opinion_id = opinion["data-entry-id"]
-                author = opinion.select_one("span.user-post__author-name").text.strip()
+                author = get_element(opinion.select_one("span.user-post__author-name").text.strip())
                 try:
                     recommendation = opinion.select_one("span.user-post__author-recomendation > em").text.strip()
                 except AttributeError:
                     recommendation = None
-                score = opinion.select_one("span.user-post__score-count").text.strip()
-                description = opinion.select_one("div.user-post__text").text.strip()
-                pros = opinion.select("div.review-feature__col:has( > div.review-feature__title--positives)> div.review-feature__item")
+                score = get_element(opinion.select_one("span.user-post__score-count").text.strip())
+                description = get_element(opinion.select_one("div.user-post__text").text.strip())
+                pros = get_element(opinion.select("div.review-feature__col:has( > div.review-feature__title--positives)> div.review-feature__item"))
                 pros = [p.text.strip() for p in pros]
                 cons = opinion.select("div.review-feature__col:has( > div.review-feature__title--negatives)> div.review-feature__item")
                 cons = [c.text.strip() for c in cons]
-                like = opinion.select_one("button.vote-yes")["data-total-vote"].strip()
-                dislike = opinion.select_one("button.vote-no")["data-total-vote"].strip()
-                publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip()
-                try:
-                    purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip()
-                except AttributeError:
-                    purchase_date = None
+                like = get_element(opinion.select_one("button.vote-yes")["data-total-vote"].strip())
+                dislike = get_element(opinion.select_one("button.vote-no")["data-total-vote"].strip())
+                publish_date = get_element(opinion.select_one("span.user-post__published > time:nth-child(1)","datetime"))
+                
                 single_opinion = {
                     "opinion-id": opinion_id,
                     "author": author,
@@ -61,6 +68,7 @@ while url:
         else:
             print(f"There are no opinions about product with {product_code} code.")
             url = None
+    print(url)
 if len(all_opinions) > 0:
     with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
         json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
